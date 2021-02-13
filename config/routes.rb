a074425d881cdb6a0c890e990aefd0e23b4865fd
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-02-13 13:04:09 -0500

    Create read all/one create actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root                   to: 'tasks#index'
+  # read all
   get 'tasks',           to: 'tasks#index'
 
+  # add new task
   get 'tasks/new',       to: 'tasks#new', as: :new_task
   post 'tasks',          to: 'tasks#create'
+
   # NB: The `show` route needs to be *after* `new` route.
+
+  # read one
   get 'tasks/:id',       to: 'tasks#show', as: :task
 
+  # edit
   get 'tasks/:id/edit',  to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id',     to: 'tasks#update'
 
+  # delete
   delete 'tasks/:id',    to: 'tasks#destroy'
 end
